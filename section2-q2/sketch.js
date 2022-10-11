854707d74f4bc3f8a18b0521e2a87a15f6e89449
2110631h <2110631h@stu.kobe-u.ac.jp>
2022-10-11 14:28:28 +0900

    change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,25 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i+j)%2==0){fill(255,255,255)}
+      else{fill(128)}
+    rect(size*i,size*j,size,size)
+    fill(255,0,0)
+    if(i%2==1){
+    let a=i+0.5;
+    circle(size*a,size*0.5,size*0.9);
+    circle(size*a,size*2.5,size*0.9);
+    fill(0)
+    circle(size*a,size*6.5,size*0.9);}
+    else{
+    let b=i+0.5
+    circle(size*b,size*1.5,size*0.9);
+    fill(0)
+    circle(size*b,size*5.5,size*0.9);
+    circle(size*b,size*7.5,size*0.9);}
+  }
     }
   }
-}
+
+
+circle(size*1.5,size*0.5,size*0.9)
